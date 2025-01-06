d6b27eca399b651b79f02c7cf6c8af78f3889d01
alephseven <rahul.kumar@eminds.ai>
2025-01-06 15:09:01 +0530

    Enabled only required plugin

--- config/plugins.js
+++ config/plugins.js
@@ -15,37 +15,37 @@ module.exports = ({ env }) => ({
       enabled: true,
       resolve: "./src/plugins/plugins/beckn-trade-bpp", // path to the plugin folder,
     },
-    "driver-app": {
-      enabled: true,
-      resolve: "./src/plugins/plugins/driver-app", // path to the plugin folder,
-    },
-    "industry-plugin": {
-      enabled: true,
-      resolve: "./src/plugins/plugins/industry-plugin", // path to the plugin folder,
-    },
+    // "driver-app": {
+    //   enabled: true,
+    //   resolve: "./src/plugins/plugins/driver-app", // path to the plugin folder,
+    // },
+    // "industry-plugin": {
+    //   enabled: true,
+    //   resolve: "./src/plugins/plugins/industry-plugin", // path to the plugin folder,
+    // },
     "users-permissions": {
       config: {
         jwt: {
-          expiresIn: "2h",
+          expiresIn: "24h",
         },
       },
     },
-    "unsolicited-request-plugin": {
-      enabled: true,
-      resolve: "./src/plugins/plugins/unsolicited-request-plugin",
-    },
-    'policy-api': {
-      enabled: true,
-      resolve: './src/plugins/plugins/policy-api'
-    },
-    'policy-violation': {
-      enabled: true,
-      resolve: './src/plugins/plugins/policy-violation'
-    },
-    "ptop-energy": {
-      enabled: true,
-      resolve: "./src/plugins/plugins/ptop-energy", // path to the plugin folder,
-    },
+    // "unsolicited-request-plugin": {
+    //   enabled: true,
+    //   resolve: "./src/plugins/plugins/unsolicited-request-plugin",
+    // },
+    // 'policy-api': {
+    //   enabled: true,
+    //   resolve: './src/plugins/plugins/policy-api'
+    // },
+    // 'policy-violation': {
+    //   enabled: true,
+    //   resolve: './src/plugins/plugins/policy-violation'
+    // },
+    // "ptop-energy": {
+    //   enabled: true,
+    //   resolve: "./src/plugins/plugins/ptop-energy", // path to the plugin folder,
+    // },
     'email': {
       config: {
         provider: 'sendgrid',
