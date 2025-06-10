bebe2633156fd2864ddf778daeaa3460c33cdce2
shreyvishal <shreya.vishal@eminds.ai>
2025-06-10 17:34:32 +0530

    Fixed Script for database setup

--- strapi-setup/setup/setup-database.sh
+++ strapi-setup/setup/setup-database.sh
@@ -7,9 +7,10 @@ while ! docker exec mysql-container mysqladmin ping -h localhost -u root -proot 
 done
 echo "MySQL is ready!"
 sleep 5
-docker cp ./strapi-setup/setup/strapi_dump.sql mysql-container:/strapi_dump.sql
-docker exec -i mysql-container mysql -uroot -proot -e "CREATE USER 'root'@'%' IDENTIFIED BY 'root'; GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+
+docker exec -i mysql-container mysql -uroot -proot -e "DROP USER IF EXISTS 'root'@'%'; CREATE USER 'root'@'%' IDENTIFIED BY 'root'; GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 
 docker exec -i mysql-container mysql -uroot -proot -e "CREATE DATABASE strapi_deg_local;"
-docker exec -i mysql-container mysql -hlocalhost -uroot -proot strapi_deg_local < strapi_dump.sql
+docker cp strapi-setup/setup/strapi_dump.sql mysql-container:/strapi_dump.sql
+docker exec -i mysql-container mysql -hlocalhost -uroot -proot strapi_deg_local < strapi-setup/setup/strapi_dump.sql
 echo "Database loaded successfully"
